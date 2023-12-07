c6356f2cbbb990bfa286c26dc36c5bb301cc325e
Msabariswaran <140327782+Msabariswaran@users.noreply.github.com>
2023-12-07 09:13:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Tiktactoc from "./Componets/Tiktactoc/Tiktactoc";
+
+
+function App() {
+  return (
+    <div className="App">
+   
+      <Tiktactoc/>
+    </div>
+  );
+}
+
+export default App;
